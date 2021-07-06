60f90916acdd1d4e9bda65345762994db719c324
elvissoftnife <elvissoftnife28@hotmail.com>
2021-07-06 04:34:41 -0500

    configuracion inicial

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-btn depressed color="primary" v-on:click="iniciarSesion()">
+        Iniciar Sesion
+      </v-btn>
+      <p>Login</p>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "login",
+  components: {},
+  methods: {
+    iniciarSesion() {
+      this.$router.push("/home");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
