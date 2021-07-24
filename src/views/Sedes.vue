d311664883910dc2b357520ac01855d9057f5e66
unknown <vegachavezjhordan@gmail.com>
2021-07-23 20:45:31 -0500

    fix: corregir columnas de tabla y solocionar warnings

--- src/views/Sedes.vue
+++ src/views/Sedes.vue
@@ -17,144 +17,13 @@
               vertical
             ></v-divider>
             <v-spacer></v-spacer>
-            <v-dialog
-              v-model="dialog"
-              max-width="900px"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  color="primary"
-                  dark
-                  class="mb-2"
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  Agregar Sede
-                </v-btn>
-              </template>
+            <v-dialog v-model="dialogVisible" max-width="500px">
               <v-card>
-                <v-card-title>
-                  <span class="text-h5">{{ formTitle }}</span>
-                </v-card-title>
-
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                        class="py-0"
-                      >
-                        <v-text-field
-                          v-model="editedItem.nombre"
-                          label="Nombre"
-                          dense
-                          outlined
-                          :rules="[rules.requiredHard('Nombre')]"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                        class="py-0"
-                      >
-                        <v-select
-                          v-model="editedItem.agencia"
-                          label="Agencia"
-                          dense
-                          outlined
-                          :rules="[rules.requiredHard('Agencia')]"
-                          :menu-props="{ bottom: true, offsetY: true }"
-                        ></v-select>
-                      </v-col>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                        class="py-0"
-                      >
-                        <v-text-field
-                          v-model="editedItem.contactame"
-                          label="Contacto"
-                          dense
-                          outlined
-                          :rules="[rules.requiredHard('Contacto')]"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                        class="py-0"
-                      >
-                        <v-text-field
-                          v-model="editedItem.direccion"
-                          label="Dirección"
-                          dense
-                          outlined
-                          :rules="[rules.requiredHard('Dirección')]"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                        class="py-0"
-                      >
-                        <v-text-field
-                          v-model="editedItem.distrito"
-                          label="Distrito"
-                          dense
-                          outlined
-                          :rules="[rules.requiredHard('Distrito')]"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col
-                        cols="12"
-                        sm="7"
-                        class="py-0"
-                      >
-                        <v-file-input
-                        v-model="editedItem.image"
-                          :rules="rules"
-                          accept="image/png, image/jpeg, image/bmp"
-                          placeholder="Seleccione una imagen"
-                          prepend-icon="mdi-camera"
-                          label="Imagen"
-                          dense
-                          outlined
-                        ></v-file-input>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-card-text>
-
+                <v-card-title class="text-h5">Estas seguro que quieres desabilitar esta sede?</v-card-title>
                 <v-card-actions>
                   <v-spacer></v-spacer>
-                  <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="close"
-                  >
-                    Cancel
-                  </v-btn>
-                  <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="save"
-                  >
-                    Guardar
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-            <v-dialog v-model="dialogDelete" max-width="500px">
-              <v-card>
-                <v-card-title class="text-h5">Estas seguro que quieres eliminar esta sede?</v-card-title>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                  <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                  <v-btn color="blue darken-1" text @click="closeVisible">Cancel</v-btn>
+                  <v-btn color="blue darken-1" text @click="changeVisible">OK</v-btn>
                   <v-spacer></v-spacer>
                 </v-card-actions>
               </v-card>
@@ -171,22 +40,9 @@
         <template v-slot:[`item.estado`]="{ item }">
           <v-simple-checkbox
             v-model="item.estado"
+            v-ripple
+            @click="openDialogVisible(item)"
           ></v-simple-checkbox>
-        </template>
-        <template v-slot:[`item.actions`]="{ item }">
-          <v-icon
-            small
-            class="mr-2"
-            @click="editItem(item)"
-          >
-            mdi-pencil
-          </v-icon>
-          <v-icon
-            small
-            @click="deleteItem(item)"
-          >
-            mdi-delete
-          </v-icon>
         </template>
         <template v-slot:no-data>
           <v-btn
@@ -204,8 +60,7 @@
 import { rules } from "@/utils/rules";
 export default {
    data: () => ({
-      dialog: false,
-      dialogDelete: false,
+      dialogVisible: false,
       headers: [
         {
           text: 'Nombre',
@@ -216,9 +71,8 @@ export default {
         { text: 'Dirección', value: 'direccion' },
         { text: 'Distrito', value: 'distrito' },
         { text: 'Agencia a la que pertenece', value: 'agencia' },
-        { text: 'Visible', value: 'estado', sortable: false },
-        { text: 'Imagen', value: 'image', sortable: false },
-        { text: 'Actions', value: 'actions', sortable: false },
+        { text: 'Imagen', value: 'image', sortable: false }, 
+        { text: 'Visible', value: 'estado', sortable: false }, 
       ],
       desserts: [],
       editedIndex: -1,
@@ -250,17 +104,12 @@ export default {
     }),
 
     computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'Agregar Sede' : 'Editar Sede'
-      },
+
     },
 
     watch: {
-      dialog (val) {
-        val || this.close()
-      },
-      dialogDelete (val) {
-        val || this.closeDelete()
+      dialogVisible (val) {
+        val || this.closeVisible()
       },
     },
 
@@ -278,7 +127,7 @@ export default {
             distrito: 'Lima',
             agencia: 'Agencia GAAA',
             image: "Imagen",
-            estado: "true",
+            estado: true,
           },
           {
             nombre: 'Ace Work and Travel-Lima',
@@ -287,7 +136,7 @@ export default {
             distrito: 'Lima',
             agencia: 'Agencia GAAA',
             image: "Imagen",
-            estado: "true",
+            estado: true,
           },
           {
             nombre: 'Ace Work and Travel-Lima',
@@ -296,7 +145,7 @@ export default {
             distrito: 'Lima',
             agencia: 'Agencia GAAA',
             image: "Imagen",
-            estado: "true",
+            estado: true,
           },
           {
             nombre: 'Ace Work and Travel-Lima',
@@ -305,7 +154,7 @@ export default {
             distrito: 'Lima',
             agencia: 'Agencia GAAA',
             image: "Imagen",
-            estado: "true",
+            estado: true,
           },
           {
             nombre: 'Ace Work and Travel-Lima',
@@ -314,7 +163,7 @@ export default {
             distrito: 'Lima',
             agencia: 'Agencia GAAA',
             image: "Imagen",
-            estado: "true",
+            estado: true,
           },
           {
             nombre: 'Ace Work and Travel-Lima',
@@ -323,7 +172,7 @@ export default {
             distrito: 'Lima',
             agencia: 'Agencia GAAA',
             image: "Imagen",
-            estado: "true",
+            estado: true,
           },
           {
             nombre: 'Ace Work and Travel-Lima',
@@ -332,7 +181,7 @@ export default {
             distrito: 'Lima',
             agencia: 'Agencia GAAA',
             image: "Imagen",
-            estado: "true",
+            estado: true,
           },
           {
             nombre: 'Ace Work and Travel-Lima',
@@ -341,7 +190,7 @@ export default {
             distrito: 'Lima',
             agencia: 'Agencia GAAA',
             image: "Imagen",
-            estado: "true",
+            estado: true,
           },
           {
             nombre: 'Ace Work and Travel-Lima',
@@ -350,7 +199,7 @@ export default {
             distrito: 'Lima',
             agencia: 'Agencia GAAA',
             image: "Imagen",
-            estado: "true",
+            estado: true,
           },
           {
             nombre: 'Ace Work and Travel-Lima',
@@ -359,7 +208,7 @@ export default {
             distrito: 'Lima',
             agencia: 'Agencia GAAA',
             image: "Imagen",
-            estado: "true",
+            estado: true,
           },
           {
             nombre: 'Ace Work and Travel-Lima',
@@ -368,52 +217,30 @@ export default {
             distrito: 'Lima',
             agencia: 'Agencia GAAA',
             image: "Imagen",
-            estado: "true",
+            estado: true,
           },
         ]
       },
 
-      editItem (item) {
+      openDialogVisible(item) {
         this.editedIndex = this.desserts.indexOf(item)
         this.editedItem = Object.assign({}, item)
-        this.dialog = true
+        this.dialogVisible = true
       },
 
-      deleteItem (item) {
-        this.editedIndex = this.desserts.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialogDelete = true
-      },
-
-      deleteItemConfirm () {
+      changeVisible () {
         this.desserts.splice(this.editedIndex, 1)
-        this.closeDelete()
+        this.closeVisible()
       },
 
-      close () {
-        this.dialog = false
+      closeVisible () {
+        this.dialogVisible = false
         this.$nextTick(() => {
           this.editedItem = Object.assign({}, this.defaultItem)
           this.editedIndex = -1
         })
       },
 
-      closeDelete () {
-        this.dialogDelete = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.desserts[this.editedIndex], this.editedItem)
-        } else {
-          this.desserts.push(this.editedItem)
-        }
-        this.close()
-      },
     },
 };
 </script>
